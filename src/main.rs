d2ba5a3d3cb8ab6822e85e802899815822e8cddc
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2021-01-11 16:07:24 -0500

    Made  configurable, and added basic error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-use rocket;
+use rocket::{self, config::Environment, Config};
 use rocket_contrib::serve::StaticFiles;
 use std::env;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() < 2 {
-		println!("args: <path>");
+		println!("args: <path> [<port>]");
 		return;
 	}
-	let static_path = &args[1];
+	let static_path: &String = &args[1];
+	let port: u16 = if args.len() >= 3 {
+		match args[2].parse::<u16>() {
+			Ok(p) => p,
+			Err(e) => {
+				eprintln!("The port value must be an integer: {}", e);
+				return;
+			}
+		}
+	} else {
+		8080
+	};
 
-	rocket::ignite()
+	let config = match Config::build(Environment::Development)
+		.address("0.0.0.0")
+		.port(port)
+		.finalize()
+	{
+		Ok(c) => c,
+		Err(e) => {
+			eprintln!("The port value is invalid: {}", e);
+			return;
+		}
+	};
+
+	let launch_error = rocket::custom(config)
 		.mount("/", StaticFiles::from(static_path))
 		.launch();
+	eprintln!("There was an error launching the server: {}", launch_error);
 }
