f06d60b6d0ee1887285f63299e6bfc33c6bada57
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2021-10-09 17:29:39 -0400

    Applied template changes to the project configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+//! A basic file server made with [Rocket](https://rocket.rs/) for serving up static files over HTTP.
+
+// Linting rules
+#![warn(
+	missing_crate_level_docs,
+	missing_docs,
+	clippy::complexity,
+	clippy::correctness,
+	clippy::perf,
+	clippy::style,
+	clippy::suspicious,
+	clippy::pedantic,
+	clippy::filetype_is_file,
+	clippy::str_to_string
+)]
+#![allow(
+	dead_code,
+	unused_macros,
+	clippy::cast_possible_truncation,
+	clippy::cast_possible_wrap,
+	clippy::cast_precision_loss,
+	clippy::cast_sign_loss,
+	clippy::doc_markdown,
+	clippy::module_name_repetitions,
+	clippy::similar_names,
+	clippy::too_many_lines,
+	clippy::unnecessary_wraps
+)]
+
 mod cli;
 mod favicon;
 
